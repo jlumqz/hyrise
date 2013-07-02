4f044aeb264a6cc5fbbb9367490835b68089963a
bastih <sebastian.hillig@gmail.com>
2013-07-02 10:45:33 +0200

    Cleanup whitepsace in TXContext.h

--- src/lib/io/TXContext.h
+++ src/lib/io/TXContext.h
@@ -4,24 +4,20 @@
 
 #include <helper/types.h>
 
-namespace hyrise {namespace tx{
-
+namespace hyrise { namespace tx {
 
 struct TXContext {
+  using id_t = hyrise::tx::transaction_id_t;
 
-	using id_t = hyrise::tx::transaction_id_t;
+  hyrise::tx::transaction_id_t tid = hyrise::tx::UNKNOWN;
+  hyrise::tx::transaction_id_t lastCid = hyrise::tx::UNKNOWN;
+  hyrise::tx::transaction_id_t cid = hyrise::tx::UNKNOWN;
 
-	hyrise::tx::transaction_id_t tid = hyrise::tx::UNKNOWN;
-	hyrise::tx::transaction_id_t lastCid = hyrise::tx::UNKNOWN;
-	hyrise::tx::transaction_id_t cid = hyrise::tx::UNKNOWN;
+  TXContext() = default;
 
-	TXContext() {}
-
-	TXContext(id_t _tid, id_t _lastCid, id_t _cid = hyrise::tx::UNKNOWN):
-	tid(_tid), lastCid(_lastCid), cid(_cid){}
-
+  TXContext(id_t _tid, id_t _lastCid, id_t _cid = hyrise::tx::UNKNOWN):
+      tid(_tid), lastCid(_lastCid), cid(_cid) {}
 };
-
 
 }}
 
